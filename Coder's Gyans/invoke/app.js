1234ae654220c387b66b84e4dd52e07a1a0d7479
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-09-29 20:44:28 +0530

    Backup

--- Coder's Gyans/invoke/app.js
+++ Coder's Gyans/invoke/app.js
@@ -4,15 +4,26 @@ const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
 
 async function main() {
   const completation = await groq.chat.completions.create({
+    temperature: 1,
+    // top_p: 1,
+    // stop: '7',
+    // max_completion_tokens: 5,
+    // frequency_penalty: 0,
+    
     model: 'llama-3.3-70b-versatile',
     messages: [
       {
+        role: 'system', 
+        content: `You are Jarvis.`,
+      },
+      {
         role: 'user',
-        content: `Hi, How are you? I'm Sharif Iqbal Jamil aka grimwebdeveloper.`,
+        content: `How are you?`,
       },
     ],
   });
 
+  // console.log(completation);
   console.log(completation.choices[0].message);
 }
 
