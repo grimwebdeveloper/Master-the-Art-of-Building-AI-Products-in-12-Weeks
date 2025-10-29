72aebc07a992288f581bf379e4e541a761d63b69
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-10-29 15:11:14 +0530

    #1 document load

---
+++ rag project/prepare.js
@@ -0,0 +1,7 @@
+import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+
+export async function indexTheDocument(filePath) {
+	const loader = new PDFLoader(filePath, {splitPages: false});
+	const docs = await loader.load();
+	console.log(docs[0].pageContent);
+}
