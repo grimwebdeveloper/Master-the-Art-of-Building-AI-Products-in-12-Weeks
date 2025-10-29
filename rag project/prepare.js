2ac33decf2e96feeaece8c1fbb185cca7ab32ce1
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-10-29 15:32:39 +0530

    #2 chunks

--- rag project/prepare.js
+++ rag project/prepare.js
@@ -1,7 +1,15 @@
 import { PDFLoader } from '@langchain/community/document_loaders/fs/pdf';
+import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
 
 export async function indexTheDocument(filePath) {
-	const loader = new PDFLoader(filePath, {splitPages: false});
+	const loader = new PDFLoader(filePath, { splitPages: false });
 	const docs = await loader.load();
-	console.log(docs[0].pageContent);
+
+	const textSplitter = new RecursiveCharacterTextSplitter({
+		chunkSize: 500,
+		chunkOverlap: 100,
+	});
+
+	const texts = await textSplitter.splitText(docs[0].pageContent);
+	console.log(texts);
 }
